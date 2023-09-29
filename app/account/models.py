3acdb9204bfaa2db64f9022b332194db25270c47
defAutumn <chromweechannelusa@gmail.com>
2023-09-29 21:56:05 +0300

    Added CustomUser model + Argon2

--- app/account/models.py
+++ app/account/models.py
@@ -1,3 +1,60 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, first_name: str, last_name: str, email: str,
+                    password: str = None, is_staff=False, is_superuser=False) -> 'CustomUser':
+        if not email:
+            raise ValueError('User must have an email')
+        if not first_name:
+            raise ValueError('User must have a first name')
+        if not last_name:
+            raise ValueError('User must have a last name')
+
+        user = self.model(email=self.normalize_email(email))
+        user.first_name = first_name
+        user.last_name = last_name
+        user.set_password(password)
+        user.is_active = True
+        user.is_staff = is_staff
+        user.is_superuser = is_superuser
+        user.save()
+
+        return user
+
+    def create_superuser(self, first_name: str, last_name: str,
+                         email: str, password: str = None) -> 'CustomUser':
+        user = self.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+            is_staff=True,
+            is_superuser=True
+        )
+        user.save()
+
+        return user
+
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    is_verified = models.BooleanField(default=False)
+    otp = models.CharField(max_length=6, null=True, blank=True)
+    login_date = models.CharField(max_length=50, null=True, blank=True)
+    username = None
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
